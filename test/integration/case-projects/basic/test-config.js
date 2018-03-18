2b09836cdd9dd0e2e176aee6e9033fbdd91e8cd4
Alexander Kachkaev <alexander@kachkaev.ru>
2018-03-18 10:04:17 +0000

    Add tests for wrong command arguments

--- test/integration/case-projects/basic/test-config.js
+++ test/integration/case-projects/basic/test-config.js
@@ -8,10 +8,4 @@ module.exports = [{
   args: ['Main.elm', 'superfluous', 'arguments'],
   expectedStdout: 'static output\n',
   expectedStderr: '',
-}, {
-  title: 'non-existing module',
-  args: ['NonExistingModule.elm'],
-  expectedExitCode: 1,
-  expectedStdout: '',
-  expectedStderr: ({ projectDir }) => `Error: File '${projectDir}/NonExistingModule.elm' does not exist\n`,
 }];
