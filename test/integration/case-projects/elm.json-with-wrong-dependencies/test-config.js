82c45d18e73c30872ca3e9abd7d5b3f1f2bbfaac
Alexander Kachkaev <alexander@kachkaev.ru>
2018-09-17 22:16:51 +0100

    Change how windows paths are escaped in tests

--- test/integration/case-projects/elm.json-with-wrong-dependencies/test-config.js
+++ test/integration/case-projects/elm.json-with-wrong-dependencies/test-config.js
@@ -3,6 +3,7 @@ const homeDir = require('os').homedir();
 const elmDir = process.platform === 'win32'
   ? `${homeDir}\\AppData\\Roaming\\elm`.replace(/\\/g, '\\\\')
   : `${homeDir}/.elm`;
+const elmDirInJson = elmDir.replace(/\\/, '\\\\');
 
 // The value for elmDir may vary in length, which may affect line endings in error messages.
 // Soft line breaks in regexps are replaced with _ for code readability.
@@ -31,6 +32,6 @@ This suggests that your ${elmDir}${_}directory${_}has${_}been${_}corrupted.${_}M
     cleanElmStuff: true,
     expectedExitCode: 1,
     expectedError: new RegExp(`^Compilation failed
-{"type":"error","path":null,"title":"CORRUPT CACHE","message":\\["I ran into an unknown package while exploring dependencies:\\\\n\\\\n    ",{"bold":false,"underline":false,"color":"yellow","string":"dev\\/null"},"\\\\n\\\\nThis suggests that your ${elmDir.replace('\\', '\\\\')}${_}directory${_}has${_}been${_}corrupted.${_}Maybe${_}some${_}program${_}is${_}messing${_}with${_}it\\?${_}It${_}is${_}just${_}cached${_}files,${_}so${_}you${_}can${_}delete${_}it${_}and${_}see${_}if${_}that${_}fixes${_}the${_}issue\\."\\]}$`)
+{"type":"error","path":null,"title":"CORRUPT CACHE","message":\\["I ran into an unknown package while exploring dependencies:\\\\n\\\\n    ",{"bold":false,"underline":false,"color":"yellow","string":"dev\\/null"},"\\\\n\\\\nThis suggests that your ${elmDirInJson}${_}directory${_}has${_}been${_}corrupted.${_}Maybe${_}some${_}program${_}is${_}messing${_}with${_}it\\?${_}It${_}is${_}just${_}cached${_}files,${_}so${_}you${_}can${_}delete${_}it${_}and${_}see${_}if${_}that${_}fixes${_}the${_}issue\\."\\]}$`)
   }
 ];
